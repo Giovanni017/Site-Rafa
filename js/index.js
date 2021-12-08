7e559dc3315afbc467e1390c7b1be861c9314079
Giovanni017 <92633222+Giovanni017@users.noreply.github.com>
2021-12-08 11:30:03 -0300

    [WIP] adição de conteudo
    adições de conteudos e do menu lateral

--- js/index.js
+++ js/index.js
@@ -13,3 +13,20 @@ if(pontos.style.display === "none"){
     btnLeiaMais.innerHTML="Leia Menos"
 }
 }
+
+function vejaMais(){
+    var pontos=document.getElementById("ponto");
+    var maisTexto=document.getElementById("btn");
+    var btnLeiaMais=document.getElementById("btnLeia")
+    
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none"
+        btnLeiaMais.innerHTML="Leia Mais"
+    }else{
+        pontos.style.display="none"
+        maisTexto.style.display="inline"
+        btnLeiaMais.innerHTML="Leia Menos"
+    }
+    }
+    
