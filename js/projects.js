21fc00fe6e2689a4b3a0cffba91672ccdcb5a84e
Giovanni017 <92633222+Giovanni017@users.noreply.github.com>
2021-12-16 12:02:28 -0300

    finalização pagina de projetos e algumas atts

---
+++ js/projects.js
@@ -0,0 +1,31 @@
+function leiaMais(){
+    var pontos=document.getElementById("pontos");
+    var maisTexto=document.getElementById("mais");
+    var btnLeiaMais=document.getElementById("btnLeiaMais")
+    
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none"
+        btnLeiaMais.innerHTML="Leia Mais"
+    }else{
+        pontos.style.display="none"
+        maisTexto.style.display="inline"
+        btnLeiaMais.innerHTML="Leia Menos"
+    }
+}
+
+function vejaMais(){
+    var pontos=document.getElementById("ponto");
+    var maisTexto=document.getElementById("btn");
+    var btnLeiaMais=document.getElementById("btnLeia")
+    
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none"
+        btnLeiaMais.innerHTML="Leia Mais"
+    }else{
+        pontos.style.display="none"
+        maisTexto.style.display="inline"
+        btnLeiaMais.innerHTML="Leia Menos"
+    }
+    }
